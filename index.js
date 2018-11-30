53c18dea9a4c565f5c1aa26c95cbdfc477543e10
Ali-Khan5 <ali_allrounder@hotmail.com>
2018-11-30 22:38:43 +0500

    express

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const Nightmare = require('nightmare');
 const nightmare = Nightmare({ show: false });
-
+const express = require("express");
+const app = express();
 const URL = 'http://blog.oscarmorrison.com/nightmarejs-on-heroku-the-ultimate-scraping-setup/';
 console.log('Welcome to Nightmare scrape\n==========');
 
-nightmare
+// nightmare
+//     .goto(URL)
+//     .wait('.post-title')
+//     .evaluate(() => document.querySelector('.post-title').textContent)
+//     .end()
+//     .then((result) => {
+//         console.log(result);
+//         console.log('=========\nAll done');
+//     })
+//     .catch((error) => {
+//         console.error('an error has occurred: ' + error);
+//     })
+//     .then(() => (console.log('process exit'), process.exit()));
+
+    app.get("/", (req, res) => {
+        res.json({ message: "Welcome to Article application." });
+        nightmare
     .goto(URL)
     .wait('.post-title')
     .evaluate(() => document.querySelector('.post-title').textContent)
@@ -17,3 +34,8 @@ nightmare
         console.error('an error has occurred: ' + error);
     })
     .then(() => (console.log('process exit'), process.exit()));
+      });
+      
+const port = process.env.PORT || 5000;
+// listen for requests
+app.listen(port, () => console.log(`Listening on port ${port}`));
